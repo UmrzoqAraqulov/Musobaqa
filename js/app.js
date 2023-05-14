a2682f6cb834baf2dcc780d731b87f21d10236e9
Umrzoq0323 <araqulovumrzoq0323@gmail.com>
2023-05-14 22:33:48 +0500

    m

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 let icon = document.getElementById("icon");
+let headerImg = document.getElementById("headerImg");
 
 icon.onclick = function () {
     document.body.classList.toggle("dark")
     if(document.body.classList.contains("dark")){
         icon.className = "fa-solid fa-sun"
+        headerImg.src = "image/imageHomePage/header_dark.png"
     }
     else{
         icon.className = "fa-solid fa-moon"
+        headerImg.src = "image/imageHomePage/header_light.png"
     }
 }
